5966a97054df2ca9a45e63e4f1a4d8df537600c6
Zain Qureshi <zuzuzain@gmail.com>
2024-10-03 17:36:50 -0400

    added python with flask environment

---
+++ app/__init__.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_cors import CORS
+
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object('app.config.Config')
+
+    db.init_app(app)
+    bcrypt.init_app(app)
+    CORS(app)
+
+    from app import routes
+    
+    return app
